8053d6c4809a29b4d644d1c60a30428c2e56ef34
marwenDevBannour <marwen.bannour.dev@gmail.com>
2022-12-26 11:42:20 +0100

    This is a new commit for what I originally planned to be amended

---
+++ BlazorTaskJob/Data/JobModel.cs
@@ -0,0 +1,19 @@
+﻿using static BlazorTaskJob.Data.JobModel;
+
+namespace BlazorTaskJob.Data
+{
+    public class JobModel
+    {
+        public int Id { get; set; }
+        public JobStatuses Status { get; set; }
+        public string Description { get; set; }
+        public DateTime LastUpdated { get; set; }
+
+    }
+    public enum JobStatuses
+    {
+        Todo,
+        Started,
+        Completed
+    }
+}
